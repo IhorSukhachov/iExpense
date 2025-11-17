a97b984f71b0ea75aa2a167ee9de698314623b92
IhorSukhachov <i.sukhachov@gmail.com>
2025-11-17 07:55:26 +0200

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Ihor Sukhachov on 17.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
