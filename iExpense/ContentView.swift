072b2580ec2aef9c2f3211d43fd784cf9e17938f
IhorSukhachov <i.sukhachov@gmail.com>
2025-11-17 08:10:46 +0200

    observable explanation

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -7,13 +7,19 @@
 
 import SwiftUI
 
+@Observable
+class User {
+    var firstName = "Ihor"
+    var lastName = "Sukhachov"
+}
 struct ContentView: View {
+    @State private var user = User()
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Your name is \(user.firstName) \(user.lastName)")
+            
+            TextField("Firstname", text: $user.firstName)
+            TextField("Lastname", text: $user.lastName)
         }
         .padding()
     }
