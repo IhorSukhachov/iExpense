0f507ad43a1ad852dd143f5b5ab20361991bbc01
IhorSukhachov <i.sukhachov@gmail.com>
2025-11-18 12:13:58 +0200

    add new  expense view

---
+++ iExpense/AddView.swift
@@ -0,0 +1,18 @@
+//
+//  AddView.swift
+//  iExpense
+//
+//  Created by Ihor Sukhachov on 18.11.2025.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddView()
+}
