96d6e93b5aa4959f7a57c421b364c5236ef61bbd
IhorSukhachov <i.sukhachov@gmail.com>
2025-11-18 10:51:08 +0200

    adding navigations stack with list

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -6,11 +6,35 @@
 //
 import SwiftUI
 
+struct ExpenseItem {
+    let name: String
+    let type: String
+    let amount: Double
+    
+    
+}
 
+@Observable
+class Expenses {
+    var items = [ExpenseItem]()
+    
+}
 
 struct ContentView: View {
+    @State private var expenses = Expenses()
+    
     var body: some View {
-        
+        NavigationStack {
+            List {
+                ForEach(expenses.items, id: \.name) { item in
+                    Text(item.name)
+                }
+            }.navigationTitle("iExpense")
+                .toolbar {
+                    
+                }
+        }
+         
     }
     
 }
