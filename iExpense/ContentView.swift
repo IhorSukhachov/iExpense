74b9290a43f0ff987833e7f6cad8348893d73d00
Ihor Sukhachov <i.sukhachov@gmail.com>
2025-12-12 11:10:06 +0200

    add model container

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -6,14 +6,6 @@
 //
 import SwiftUI
 
-struct ExpenseItem: Identifiable, Codable {
-    var id = UUID()
-    let name: String
-    let type: String
-    let amount: Double
-    
-    
-}
 
 @Observable
 class Expenses {
--- iExpense/iExpenseApp.swift
+++ iExpense/iExpenseApp.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Ihor Sukhachov on 17.11.2025.
 //
-
+import SwiftData
 import SwiftUI
 
 @main
@@ -12,6 +12,7 @@ struct iExpenseApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .modelContainer(for: [ExpenseItem.self])
         }
     }
 }
