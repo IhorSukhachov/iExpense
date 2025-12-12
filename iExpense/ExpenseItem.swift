db9c3350201c60e21c86c8b2e7fd53345080d964
Ihor Sukhachov <i.sukhachov@gmail.com>
2025-12-12 11:07:40 +0200

    add swiftdata

--- iExpense/ExpenseItem.swift
+++ iExpense/ExpenseItem.swift
@@ -4,5 +4,19 @@
 //
 //  Created by Ihor Sukhachov on 12.12.2025.
 //
-
+import SwiftData
 import Foundation
+
+class ExpenseItem: Identifiable {
+    var id: UUID = UUID()
+    var name: String
+    var type: String
+    var amount: Double
+
+    init(id: UUID = UUID(), name: String, type: String, amount: Double) {
+        self.id = id
+        self.name = name
+        self.type = type
+        self.amount = amount
+    }
+}
